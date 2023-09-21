4308829017c83881f64348be39177125f745a933
Soan1488 <andrisokolovskiy@gmail.com>
2023-09-21 20:36:03 +0200

    first task

---
+++ app.ts
@@ -0,0 +1,12 @@
+let age: number;
+age = 5
+let name1: string ;
+name1 = 'Max'
+let toggle:boolean ;
+toggle = true
+let empty: null ;
+empty = null
+let notInitialize:undefined;
+notInitialize = undefined
+let callback: (a: number) => number;
+callback = (a) => { return 100 + a };
